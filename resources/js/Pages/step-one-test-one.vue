bba40a5a2c732580c3d4022396588b9bd94db22a
ZeToMane <parente.pb@gmail.com>
2024-02-08 04:39:35 +0100

    Added p5.js

--- resources/js/Pages/step-one-test-one.vue
+++ resources/js/Pages/step-one-test-one.vue
@@ -27,7 +27,7 @@
                             <img src="../../content/arrow.svg" alt="" class="arrow-down">
                         </p>
                     </Link>
-                    <div class="canvas-container">
+                    <div id="canvas-container" ref="canvas-container" class="canvas-container">
                         
                     </div>
                 </div>
@@ -48,14 +48,134 @@
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
+import { onMounted, ref } from 'vue';
 import { Link, router, usePage } from '@inertiajs/vue3';
 import { synthName} from '../global.js';
+import p5 from 'p5';
 
 components:{
     Link
 }
 
+const p5canvas = ref(null);
+
+// const createSketch = () => {
+//     new p5((p) => {
+//         let canvas;
+//         let containerWidth;
+//         let containerHeight;
+
+//         p.setup = () => {
+//             containerWidth = document.getElementById('canvas-container').clientWidth;
+//             // containerHeight = document.getElementById('canvas-container').clientHeight;
+//             containerHeight = 206;
+//             canvas = p.createCanvas(containerWidth, containerHeight);
+//             canvas.parent('canvas-container'); // Attach canvas to the container
+//             p.background(220);
+//         };
+
+//         p.draw = () => {
+//             // Drawing logic here
+//             p.background(100);
+//         };
+//     });
+// };
+
+const createSketch = (p) => {
+    new p5((p) => {
+            let canvas;
+            let containerWidth;
+            let containerHeight;
+            let scaleTri = 200;
+
+        p.setup = () => {
+            // p.setAttributes('antialias', false); // Setting antialias attribute
+            // p.pixelDensity(0.5);
+            containerWidth = document.getElementById('canvas-container').clientWidth;
+            // containerHeight = document.getElementById('canvas-container').clientHeight;
+            containerHeight = 206;
+            canvas = p.createCanvas(containerWidth, containerHeight, p.WEBGL);
+            canvas.parent('canvas-container');
+            p.cam = p.createCamera();
+            p.cam.lookAt(scaleTri / 2, -scaleTri/3, -scaleTri / 2);
+        };
+
+        p.draw = () => {
+            p.orbitControl(1, 0, 1);
+
+            p.background(255);
+
+            //p.fill(0);
+
+            // Draw your 3D scene here
+            // First face
+            p.fill(255); // Set fill color to yellow
+            p.beginShape();
+            p.vertex(0, 0, 0);
+            p.vertex(scaleTri / 2, -scaleTri, -scaleTri / 2);
+            p.vertex(scaleTri, 0, 0);
+            p.endShape();
+
+            p.beginShape();
+            p.vertex(0, 0, -scaleTri);
+            p.vertex(scaleTri / 2, -scaleTri, -scaleTri / 2);
+            p.vertex(scaleTri, 0, -scaleTri);
+            p.endShape();
+
+            p.beginShape();
+            p.vertex(0, 0, 0);
+            p.vertex(scaleTri / 2, -scaleTri, -scaleTri / 2);
+            p.vertex(0, 0, -scaleTri);
+            p.endShape();
+
+            p.beginShape();
+            p.vertex(scaleTri, 0, 0);
+            p.vertex(scaleTri / 2, -scaleTri, -scaleTri / 2);
+            p.vertex(scaleTri, 0, -scaleTri);
+            p.endShape();
+
+            // Draw the lines
+            p.line(scaleTri, 0, 0, scaleTri, 0, -scaleTri);
+            p.line(0, 0, 0, 0, 0, -scaleTri);
+
+            let rectX = 0;
+            let rectY = 0;
+            let rectZ = 0;
+
+            p.push();
+            p.fill(0);
+            p.translate(rectX, rectY, rectZ);
+            p.rotateY(p.atan2(p.cam.eyeX - rectX, p.cam.eyeZ - rectZ));
+            p.rotateX(-p.atan2(p.cam.eyeY - rectY, p.dist(p.cam.eyeX, p.cam.eyeZ, rectX, rectZ)));
+            p.rect(0, 0, scaleTri / 2, 50);
+            p.pop();
+
+            p.push();
+            p.fill(0);
+            p.translate(rectX + scaleTri, rectY, rectZ);
+            p.rotateY(p.atan2(p.cam.eyeX - rectX + scaleTri, p.cam.eyeZ - rectZ));
+            p.rotateX(-p.atan2(p.cam.eyeY - rectY, p.dist(p.cam.eyeX, p.cam.eyeZ, rectX + scaleTri / 2, rectZ)));
+            p.rect(0, 0, scaleTri / 2, 50);
+            p.pop();
+
+            p.push();
+            p.fill(0);
+            p.translate(rectX, rectY, rectZ - scaleTri);
+            p.rotateY(p.atan2(p.cam.eyeX - rectX, p.cam.eyeZ - rectZ - scaleTri));
+            p.rotateX(-p.atan2(p.cam.eyeY - rectY, p.dist(p.cam.eyeX, p.cam.eyeZ, rectX, rectZ - scaleTri)));
+            p.rect(0, 0, scaleTri / 2, 50);
+            p.pop();
+
+            p.push();
+            p.fill(0);
+            p.translate(rectX + scaleTri, rectY, rectZ - scaleTri);
+            p.rotateY(p.atan2(p.cam.eyeX - rectX + scaleTri, p.cam.eyeZ - rectZ - scaleTri));
+            p.rotateX(-p.atan2(p.cam.eyeY - rectY, p.dist(p.cam.eyeX, p.cam.eyeZ, rectX + scaleTri / 2, rectZ - scaleTri)));
+            p.rect(0, 0, scaleTri / 2, 50);
+            p.pop();
+        };
+    });
+};
 
 onMounted(() => {
     const insertSynthName = document.querySelectorAll('.insert-synth');
@@ -63,5 +183,9 @@ onMounted(() => {
     insertSynthName.forEach((element) => {
         element.innerText = synthName['_value'];
     });
+
+    // new p5(this.createSketch, this.$refs.canvas-container);
+    createSketch();
 });
+
 </script>
